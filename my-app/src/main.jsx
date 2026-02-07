59e9a5b728fe14fee2f8a9cd5b5650649fba9ac7
Ayush Agnihotri <omg.uno.reverse@gmail.com>
2026-02-07 17:22:30 +0530

    Update main.jsx to import dark-mode-styles.css

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,17 +1,4 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary'
-import { initSentry } from './services/monitoring'
+import './index.css';
+import './components/ui/dark-mode-styles.css';
 
-// Initialize Sentry error monitoring (before React renders)
-initSentry();
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>,
-)
+// ... rest of your existing code
