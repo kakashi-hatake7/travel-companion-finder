2ffa316d34e8405b4fd700b423cafa9907596bf6
Ayush Agnihotri <omg.uno.reverse@gmail.com>
2026-02-07 17:24:18 +0530

    Update App.jsx to include DarkModeToggle component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DarkModeToggle from './DarkModeToggle';
+
+function App() {
+  return (
+    <div>
+      {/* Other components */} 
+      <ToastContainer />
+      <DarkModeToggle />
+      {/* Other components */} 
+    </div>
+  );
+}
+
+export default App;
